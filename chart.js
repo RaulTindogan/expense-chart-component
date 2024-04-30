9cbc058c77653d3ed68e4fb1468483fa1b96e3fc
RaulTindogan <tindoganraul@gmail.com>
2024-04-30 21:47:39 +0800

    initial commit

---
+++ chart.js
@@ -0,0 +1,60 @@
+let allData = [];
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    dataChart(data)
+  })
+
+function dataChart (data) {
+  const ctx = document.getElementById('myChart');
+  
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: data.map(days=>(days.day)),
+    datasets: [
+      {
+        label: 'Spending -Last 7 days',
+        data: data.map(days=>(days.amount)),
+        borderWidth: 1,
+        borderRadius: 5,
+        barPercentage: 1.1,
+        backgroundColor: [
+                'hsl(10, 79%, 65%)', // Original color for all bars
+                'hsl(10, 79%, 65%)', // Blue
+                'hsl(186, 34%, 60%)', // Yellow
+                'hsl(10, 79%, 65%)', // Green
+                'hsl(10, 79%, 65%)', // Purple
+                'hsl(10, 79%, 65%)', // Purple
+                'hsl(10, 79%, 65%)', // Purple
+        ],
+      },    
+    ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+        x: {
+          grid: {
+            display: false
+          }
+        },
+        y: {
+          grid: {
+            display: false
+          },  
+          display: false
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+  }
+ 
+});
+}
---
+++ tailwindconfig.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            "Soft-red": "hsl(10, 79%, 65%)",
+            "Cyan": "hsl(186, 34%, 60%)",
+            "Dark-brown": "hsl(25, 47%, 15%)",
+            "Medium-brown": "hsl(28, 10%, 53%)",
+            "Cream": "hsl(27, 66%, 92%)",
+            "Very-pale-orange": "hsl(33, 100%, 98%)"
+        },
+        fontFamily: {
+            "Franklin": ["Libre Franklin", "sans-serif"]
+        }
+      }
+    }
+  }
